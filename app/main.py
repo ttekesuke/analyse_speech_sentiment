4c0eedfd6588c938bb346b7a9d65d90773c03766
ttekesuke <tekesuke1986@hotmail.com>
2025-02-13 23:40:44 +0900

    added train model file

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
 from fastapi import FastAPI, WebSocket, Request
 from starlette.responses import HTMLResponse
-from transformers import pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from fastapi.templating import Jinja2Templates
 import json
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
-# Hugging Face の感情分析モデルをロード
-classifier = pipeline(
-    task="sentiment-analysis",
-    model="koheiduck/bert-japanese-finetuned-sentiment",
-    tokenizer="cl-tohoku/bert-base-japanese-whole-word-masking"
-)
+# Dockerコンテナ内のパスにある学習済みモデルをロード
+model_path = "/app/trained_model"
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
+# ソフトマックス関数
+def np_softmax(x):
+    f_x = np.exp(x) / np.sum(np.exp(x))
+    return f_x
+
+@app.get("/predict/")
+def predict(text: str):
 
 # WebSocketエンドポイント
 @app.websocket("/ws")
@@ -22,10 +27,15 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         text = await websocket.receive_text()
         if text:
-            result = classifier(text)[0]
-            if result["label"] == "NEGATIVE":
-                result["score"] *= -1  # NEGATIVE の場合はスコアを反転
-            await websocket.send_text(json.dumps({"score": round(result["score"], 2), "text": text}))
+            tokens = tokenizer(text, truncation=True, return_tensors="pt")
+            tokens.to(model.device)
+            preds = model(**tokens)
+            prob = np_softmax(preds.logits.cpu().detach().numpy()[0])
+
+            emotion_names_jp = ["joy", "sadness","anger","surprise","disgust","trust","expectation","fear"]
+            result = {n: p for n, p in zip(emotion_names_jp, prob)}
+
+            await websocket.send_text(json.dumps({"score": result, "text": text}))
 
 # HTML応答
 @app.get("/", response_class=HTMLResponse)
